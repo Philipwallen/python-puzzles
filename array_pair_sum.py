59124c4aa50d5ebb83b04db453be624a8467af27
philipwallen <philipwallen1@gmail.com>
2017-07-02 16:28:34 -0400

    1 more puzzle

---
+++ array_pair_sum.py
@@ -0,0 +1,26 @@
+def sum_pairs(arr,k):
+	
+	# check the length of the arr is > 2
+	if len(arr) < 2:
+		return 'invalid input' 
+	
+	# create sets for tracking
+	seen = set()
+	output = set()
+	
+	for elems in arr:
+		target = k - elems
+		
+		if target not in seen:
+			seen.add(elems)
+		else:
+			output.add( (min(elems, target), (max(elems, target))))
+			
+	return '\n'.join(map(str,list(output)))
+	
+
+	
+'''
+Given an integer array, output all the unique pairs that sum up to a 
+specific value k.
+'''
